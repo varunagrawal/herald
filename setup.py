a70908938dbb9dc31f95721ec2fd52359bf276ce
Varun Agrawal <varagrawal@gmail.com>
2017-08-04 20:44:59 -0400

    files needed to create package

---
+++ setup.py
@@ -0,0 +1,52 @@
+"""
+Automatically send notifications at the end of a long running task
+"""
+from setuptools import find_packages, setup
+from codecs import open
+from os import path
+import herald
+
+dependencies = []
+
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='herald',
+    version=herald.__version__,
+    url='https://github.com/varunagrawal/nuke',
+    license=herald.__license__,
+    author=herald.__author__,
+    author_email=herald.__email__,
+    description='Command line tool to nuke a directory',
+    long_description=long_description,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
